ff3b71d9c4304a22454d308eb6624e2d4850131b
alinasandybaeva <sand-alina2412@yandex.ru>
2020-05-06 22:11:46 +0300

    Added tests to UserRestaurantControllerTest

--- src/test/java/ru/sandybaeva/restaurant/web/restaurant/UserRestaurantControllerTest.java
+++ src/test/java/ru/sandybaeva/restaurant/web/restaurant/UserRestaurantControllerTest.java
@@ -43,4 +43,26 @@ class UserRestaurantControllerTest extends AbstractControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
         RESTAURANT_MATCHER.assertMatch(restaurantService.getByIdCurrent(LocalDate.now(), RESTAURANT_ID_3), RESTAURANT_3);
     }
+
+    @Test
+    void getUnauth() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void getNotFound() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "/1")
+                .with(userHttpBasic(USER_1)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    void getNotFoundRestaurantWithMenuToday() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + "/" + RESTAURANT_ID_1)
+                .with(userHttpBasic(USER_1)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
 }
